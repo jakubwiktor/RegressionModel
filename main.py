cdd5a80e55f107024ac13b3ae2eae64c725e1b65
Jakub Wiktor <jakwiktor@gmail.com>
2022-11-18 13:10:44 +0100

    updates to the model initialization

--- main.py
+++ main.py
@@ -8,49 +8,66 @@ from utils import nn
 
 def main():
 
-    plt.figure(1)
-    target = target_data(shape=32,coverage=40,plot_flag=True)
-
+    plt.figure(num=1,figsize=(5, 5))
+    target = target_data(shape=32,coverage=75,plot_flag=True)
+    plt.savefig(f"target_data.png",)
+    # return
     
     model = nn.Model()
-    loss = nn.SquareLoss()
 
     t1 = time.time()
-    for epoch in range(1,100):
-        print(epoch)
-        plt.figure(2)
-        show_prediction(shape=32,model=model)
+    
+    batch_size = 128
+
+    for epoch in range(1,1000):
         
+        if epoch%10 == 0:
+            plt.figure(num=2,figsize=(5, 5))
+            show_prediction(shape=32,model=model)
+
+            plt.savefig(f"{epoch}.png",)
+
         shuffle(target)
 
+        inputx = []
+        inputy = []
+
         for t_counter, t in enumerate(target):
-            model.train([t[0],t[1]],t[2])
             
-            if t_counter %50:
-                print( loss.loss( model.forward([t[0],t[1]]), t[2]) ) 
-        
+            inputx.append([t[0],t[1]])
+            inputy.append(t[2])
+
+            if t_counter % batch_size == 0 and t_counter > 0:
+                running_loss = model.train(inputx,inputy)
+                
+                print(running_loss)
+                
+                inputx = []
+                inputy = []
 
     print(time.time()-t1)
 
-def show_prediction(shape, model):
+def show_prediction(shape, model,scaling=1):
     
-    # fig = plt.figure()
-    
-    xspace = [x for x in range(shape) for y in range(shape)]
-    yspace = [y for x in range(shape) for y in range(shape)]
-    counter = 0
+    plt.clf()
+
+    cmap = np.zeros((shape*scaling,shape*scaling))
+    # xspace = [x/2 for x in range(shape*2) for y in range(shape*2)]
+    # yspace = [y/2 for x in range(shape*2) for y in range(shape*2)]
+
+    xspace = [x/shape for x in range(shape*scaling) for y in range(shape*scaling)]
+    yspace = [y/shape for x in range(shape*scaling) for y in range(shape*scaling)]
+
     for x,y in zip(xspace,yspace):
-        a,b = model.forward([x,y])
-        if a>b:
-            color = 'xr'
+        res = model.forward([x,y])
+        # cmap[int(y*2),int(x*2)] = res[0]/res[1]
+        if res[0]>res[1]:
+            cmap[int(y*shape*scaling),int(x*shape*scaling)] = 1
         else:
-            color = 'xb'
-        
-        plt.plot(x,y,color)
-        
-        counter += 1
-    plt.show(block=False)
-    plt.pause(1)
+            cmap[int(y*shape*scaling),int(x*shape*scaling)] = 0
+    
+    plt.imshow(np.flipud(cmap))
+    plt.pause(0.01)
 
 def target_data(shape:int, coverage:int, plot_flag:bool) -> list:
 
@@ -62,15 +79,36 @@ def target_data(shape:int, coverage:int, plot_flag:bool) -> list:
     for x in range(shape):
         for y in range(shape):
             if np.random.randint(1,101) < coverage:
-                tmp = [x+np.random.randn()/5,y+np.random.randn()/5]
-                res.append(tmp)
+                
+                xx = np.array(x+np.random.randn()/5)/shape
+                xx.astype(np.float64)
 
-    #compute 'soft' sigmoidal boundary for class assignment, class is a 2 component vector
+                yy = np.array(y+np.random.randn()/5)/shape
+                yy.astype(np.float64)
+
+                res.append([xx,yy])
+    
+
+    # # compute 'soft' sigmoidal boundary for class assignment, class is a 2 component vector
+    # for xy in res:
+    #     if xy[1] < 0.25*np.sin(16*xy[0]) - xy[0] + 1:
+    #         xy.append([1,0])
+    #     else:
+    #         xy.append([0,1])
+
+    # circle
     for xy in res:
-        if xy[1] < 5*np.sin(0.5*xy[0]) - 0.25*xy[0] + shape/2:
-            xy.append([1,0])
+        if np.sqrt((xy[0]-0.3)**2 + (xy[1]-0.3)**2) < 0.3:
+            xy.append(np.array([1,0],dtype=np.float64))
         else:
-            xy.append([0,1])
+            xy.append(np.array([0,1],dtype=np.float64))
+
+    #just line
+    # for xy in res:
+    #     if xy[1] > xy[0] + 1:
+    #         xy.append(np.array([1,0],dtype=np.float64))
+    #     else:
+    #         xy.append(np.array([0,1],dtype=np.float64))
 
     if plot_flag:
         for x,y,z in res:
@@ -80,7 +118,7 @@ def target_data(shape:int, coverage:int, plot_flag:bool) -> list:
                 plt.plot(x,y,'go')
 
         plt.show(block=False)
-
+    plt.pause(1)
     return res
 
 if __name__ == '__main__':
--- utils/nn.py
+++ utils/nn.py
@@ -10,32 +10,25 @@ class Model():
         #define netowork architecture and layers        
         
         self.Activation = Activation()
-        self.Loss = SquareLoss()
+        self.Loss = Loss()
 
-        self.features_0 = np.zeros(num_features[0])
-        self.features_1 = np.zeros(num_features[1])
-        self.features_2 = np.zeros(num_features[2])
-        self.features_3 = np.zeros(num_features[3])
+        self.L1 = Linear(features_in=num_features[0], features_out=num_features[1]) #first + hidden
+        self.L2 = Linear(features_in=num_features[1], features_out=num_features[2]) #hidden
+        self.L3 = Linear(features_in=num_features[2], features_out=num_features[3]) #output
 
-        self.L1 = Linear(features_in=self.features_0, features_out=self.features_1) #first + hidden
-        self.L2 = Linear(features_in=self.features_1, features_out=self.features_2) #hidden
-        self.L3 = Linear(features_in=self.features_2, features_out=self.features_3) #output
-
+        self.learning_rate = 0.003
 
     def forward(self,x):
         
         #forward pass on the data
-        out = self.L1.run(x)
-        out = self.Activation.RelU(out)
-        out = self.L2.run(out)
-        out = self.Activation.RelU(out)
-        out = self.L3.run(out)
-        out = self.Activation.RelU(out)
+        out1 = self.L1.run(x)
+        out2 = self.Activation.RelU(out1)
+        out3 = self.L2.run(out2)
+        out4 = self.Activation.RelU(out3)
+        out5 = self.L3.run(out4)
+        out6 = self.Activation.Sigmoid(out5)
         
-        # print(out)
-        # max_out = sum(out)
-        # out = [x/max_out for x in out]
-        return out
+        return out6
 
     def train(self,x,y):
         #this function will take data and work on the gradients of the layers. Gradients are stored in the Linear layer class.
@@ -45,67 +38,74 @@ class Model():
 
         #x i input vector with position of datapoint data, y is a class vector
         
-        h = 0.00001 #learning rate hardcoded for now
-        learning_rate = 0.001
-        current_loss = self.Loss.loss(self.forward(x), y)
+        h = 0.000001
+        current_loss = 0
+        for one_x, one_y in zip(x,y):
+            current_loss += self.Loss.loss(self.forward(one_x), one_y)
+        # current_loss = current_loss/len(x)
 
         #get gradients for weights and biases
         for i in range(len(self.L1.weights)):
             for j in range(len(self.L1.weights[i])):
                 self.L1.weights[i][j] += h
-                new_loss = self.Loss.loss(self.forward(x), y)
-                gradient = (new_loss - current_loss)/h
-                # print(gradient)
-                self.L1.weights_gradients[i][j] = gradient*learning_rate
+                new_loss = 0
+                for one_x, one_y in zip(x,y):
+                    new_loss += self.Loss.loss(self.forward(one_x), one_y)
+                # new_loss = new_loss/len(x)
                 self.L1.weights[i][j] -= h
-
+                self.L1.weights_gradients[i][j] = (new_loss - current_loss)/h
+                
         for i in range(len(self.L2.weights)):
             for j in range(len(self.L2.weights[i])):
                 self.L2.weights[i][j] += h
-                new_loss = self.Loss.loss(self.forward(x), y)
-                gradient = (new_loss - current_loss)/h
-                self.L2.weights_gradients[i][j] = gradient*learning_rate
+                new_loss = 0
+                for one_x, one_y in zip(x,y):
+                    new_loss += self.Loss.loss(self.forward(one_x), one_y)
+                self.L2.weights_gradients[i][j] = (new_loss - current_loss)/h
                 self.L2.weights[i][j] -= h
     
         for i in range(len(self.L3.weights)):
             for j in range(len(self.L3.weights[i])):
                 self.L3.weights[i][j] += h
-                new_loss = self.Loss.loss(self.forward(x), y)
-                gradient = (new_loss - current_loss)/h
-                self.L3.weights_gradients[i][j] = gradient*learning_rate
+                new_loss = 0
+                for one_x, one_y in zip(x,y):
+                    new_loss += self.Loss.loss(self.forward(one_x), one_y)
+                self.L3.weights_gradients[i][j] = (new_loss - current_loss)/h
                 self.L3.weights[i][j] -= h
-                
+                            
         for i in range(len(self.L1.biases)):
             self.L1.biases[i] += h
-            new_loss = self.Loss.loss(self.forward(x), y)
-            gradient = (new_loss - current_loss)/h
-            self.L1.biases_gradients[i] = gradient*learning_rate
+            new_loss = 0
+            for one_x, one_y in zip(x,y):
+                new_loss += self.Loss.loss(self.forward(one_x), one_y)
+            self.L1.biases_gradients[i] = (new_loss - current_loss)/h
             self.L1.biases[i] -= h
-
+                
         for i in range(len(self.L2.biases)):
             self.L2.biases[i] += h
-            new_loss = self.Loss.loss(self.forward(x), y)
-            gradient = (new_loss - current_loss)/h
-            self.L2.biases_gradients[i] = gradient*learning_rate
+            new_loss = 0
+            for one_x, one_y in zip(x,y):
+                new_loss += self.Loss.loss(self.forward(one_x), one_y)
+            self.L2.biases_gradients[i] = (new_loss - current_loss)/h
             self.L2.biases[i] -= h
 
-    
         for i in range(len(self.L3.biases)):
             self.L3.biases[i] += h
-            new_loss = self.Loss.loss(self.forward(x), y)
-            gradient = (new_loss - current_loss)/h
-            self.L3.biases_gradients[i] = gradient*learning_rate
+            new_loss = 0
+            for one_x, one_y in zip(x,y):
+                new_loss += self.Loss.loss(self.forward(one_x), one_y)
+            self.L3.biases_gradients[i] = (new_loss - current_loss)/h
             self.L3.biases[i] -= h
 
-        self.L1.weights -= self.L1.weights_gradients
-        self.L2.weights -= self.L2.weights_gradients
-        self.L3.weights -= self.L3.weights_gradients
+        self.L1.weights -= self.L1.weights_gradients*self.learning_rate        
+        self.L2.weights -= self.L2.weights_gradients*self.learning_rate
+        self.L3.weights -= self.L3.weights_gradients*self.learning_rate
+        
+        self.L1.biases  -= self.L1.biases_gradients*self.learning_rate
+        self.L2.biases  -= self.L2.biases_gradients*self.learning_rate
+        self.L3.biases  -= self.L3.biases_gradients*self.learning_rate
 
-        self.L1.biases  -= self.L1.biases_gradients
-        self.L2.biases  -= self.L2.biases_gradients
-        self.L3.biases  -= self.L3.biases_gradients
-
-        return None
+        return current_loss
 
 #####
 # other casses
@@ -122,26 +122,28 @@ class Linear():
         self.features_in = features_in
         self.features_out = features_out
         
-        self.weights = np.random.rand(self.features_in.size, self.features_out.size)/100 #as a np matrix 1st - features_in, 2nd - features_out
-        self.biases = np.random.rand(self.features_out.size)/100
+        self.weights = np.random.randn(self.features_in,self.features_out)-0.5#as a np matrix 1st - features_in, 2nd - features_out
+        self.weights = self.weights / np.sqrt(features_in)
+        self.weights.astype(np.float64)
+
+        self.biases = np.zeros(self.features_out)
+        self.biases.astype(np.float64)
+
+        self.weights_gradients = np.zeros((self.features_in,self.features_out)) #as a np matrix 1st - features_in, 2nd - features_out
+        self.weights_gradients.astype(np.float64)
         
-        self.weights_gradients = np.zeros((self.features_in.size, self.features_out.size)) #as a np matrix 1st - features_in, 2nd - features_out
-        self.biases_gradients = np.zeros(self.features_out.size) #as a np matrix 1st - features_in, 2nd - features_out
+        self.biases_gradients = np.zeros(self.features_out) #as a np matrix 1st - features_in, 2nd - features_out
+        self.biases_gradients.astype(np.float64)
 
     def run(self, x): 
     #for each feature in 'x' multiply by relevant weights and add up, then and bias
         
-        output = np.zeros(self.features_out.size)
-        
-        for outgoing, _ in enumerate(self.features_out):
-            weighted_feature = 0            
+        output = np.zeros(self.features_out)
+        for outgoing in range(self.features_out):
             for arriving, _ in enumerate(x):
-                weighted_feature += x[arriving] * self.weights[arriving,outgoing]
-            weighted_feature += self.biases[outgoing]
-            output[outgoing] = weighted_feature
-        
+                output[outgoing] += x[arriving] * self.weights[arriving, outgoing]    
+            output[outgoing] += self.biases[outgoing]      
         return output
-
 
 class Activation():
     
@@ -150,21 +152,28 @@ class Activation():
     def __init__(self):
         pass
 
-    def RelU(self,x:float): 
-        return [max(0,n) for n in x]
+    def RelU(self,x): 
+        return [max(0,r) for r in x]
+    
+    def Sigmoid(self,x):
+        return 1/(1+np.exp(-x))
 
+    def TanH(self,x):
+        res=[]
+        for r in x:
+            res.append((np.exp(r) - np.exp(-r)) / (np.exp(r) + np.exp(-r)))
+        return res
 
-class SquareLoss():
+class Loss():
     #define loss function for neural netowrk
     
     def __init__(self):
         pass
 
-    def loss(self,x:float,y:float):
+    def loss(self,x,y):
         #loss as a square of difference between the output of the net (X) and the target data (Y). 
         #in this case the output is 0/1 score. 'X'
-
-        this_loss = np.sum([(xx-yy)**2 for xx,yy in zip(x,y)])
+        this_loss = np.sum(np.power(y-x,2))
         # print(x,y,this_loss)
         return this_loss
 
